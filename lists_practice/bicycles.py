af8ff3f3ee8fa4b42e51c4fc43426c5308d56ba4
damokappa <ddamian.sbp@gmail.com>
2019-02-17 22:47:25 -0800

    Added more lists to bicycle.py

--- lists_practice/bicycles.py
+++ lists_practice/bicycles.py
@@ -1,2 +1,6 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles)
+print(bicycles) # This prints a representation of the list, basically how it appears in the IDE
+
+print(bicycles[0]) # This displays the first item in the list/array. Everything starts at 0
+
+print(bicycles[0].title()) # You can combine previous methods into lists. 
