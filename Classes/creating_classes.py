35828ef5fc58cedd231b1f70ae9e46dea258a9db
damokappa <ddamian.sbp@gmail.com>
2019-05-13 23:42:09 -0700

    Edited __init__

--- Classes/creating_classes.py
+++ Classes/creating_classes.py
@@ -3,8 +3,6 @@ class Car:
     Car models a car w/ tires and an engine
     """
     
-    def __init__(self):
-        """
-        Docstring describing the method
-        """
-        pass
+    def __init__(self, engine, tires):
+        self.engine = engine
+        self.tires = tires
