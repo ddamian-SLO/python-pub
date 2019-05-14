57355cfcad7d998f8e1fcfbbf8debccc1dddcd77
damokappa <ddamian.sbp@gmail.com>
2019-05-13 23:49:53 -0700

    Added description method

--- Classes/creating_classes.py
+++ Classes/creating_classes.py
@@ -6,3 +6,6 @@ class Car:
     def __init__(self, engine, tires):
         self.engine = engine
         self.tires = tires
+
+    def description(self):
+        print(f"A car with an {self.engine} engine, and {self.tires} tires.")
