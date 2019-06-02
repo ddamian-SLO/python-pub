24768ccc27af6e3d8a3d79502bd147a16364eb7b
damokappa <ddamian.sbp@gmail.com>
2019-06-01 21:12:07 -0700

    Created Project Folder. Started outline for regex.

---
+++ automate_the_boring_stuff/chapter_7/find_email_and_phone_numbers.py
@@ -0,0 +1,51 @@
+# Find all email addresses and phone numbers by copying them.
+# Flow of program:
+# - Pull text copied on the clipboard
+# - Find all the phone numbers and email addresses in the text
+# - Paste the text back onto the clipboard
+# Phone number is made of three parts, 
+# (111) 111-1111
+# 123-456-7890
+# Area code separated by a dash at the end or encapsulated by parentheses, 
+# 3 digits separated with a dash at the end, 
+# and then 4 digits
+# Regex will need to detect:
+#   The area code and it's different separators
+#   The separator after the area code (either space or a dash)
+#   The 3 digits
+#   The separator again
+#   The 4 digits
+# TODO: Add Regex option to pull extensions. 
+
+# An email address is made of 2 parts: 
+#   name@domain.com
+#   Regex will need to check for:
+#       - The name before the '@' sign.
+#       - The @ sign
+#       - The full domain (including the subdomain. It should just be able to check if it ends with a character)
+# Things to consider:
+#   email addresses often have underscores or periods(.)
+#   email addresses cannot start with a special character
+#   the account name ends when it reaches @
+#   the domain begins after the @ symbol
+#   The domain ends once it reaches it's TLD
+#   The TLD has to end with a string
+
+import re, pyperclip
+
+def main():
+
+    phone_number_regex = re.compile(r'''(
+    (d{3}|\(\d{3}\))    # Area Code
+    (\s|-|\.)?          # Separator
+    \d\d\d              # 3 digits
+    (\s|-|\.)           # Separator
+    \d\d\d\d            # Last 4 digits
+    )''', re.VERBOSE)
+    
+    # TODO: Create Regex Object for Email Addresses for all matches
+
+    # TODO: Use pyperclip to pull clipboard info and assign it to variable.
+
+    # TODO: Return List of Phone Numbers and Email addresses. If none found, notify. 
+
